7903b10d58eea9f548ee4336c09f0c0b87db556e
oleg.alexandrov <oleg.alexandrov@cf5def4a-7935-0410-a750-a3d11708e98c>
2007-09-21 23:19:00 +0000

    Add rm_extra_html

--- bin/fetch_articles_cats.pl
+++ bin/fetch_articles_cats.pl
@@ -1,6 +1,7 @@
 require 'bin/get_html.pl';
 require 'bin/language_definitions.pl';
 require 'bin/escape_things.pl';
+require 'bin/rm_extra_html.pl';
 use Encode;
 
 sub fetch_articles_cats{
@@ -34,6 +35,7 @@ sub fetch_articles_cats{
 
      ($text, $error) = &get_html($link);
      $text =~ s/\<\/?b\>//ig; # rm strange bold markup in the query format
+     $text = &rm_extra_html($text);
 
      # a kind of convoluted code. Try harder to get the continuation of current category than the first page
      if ($link =~ /cpfrom=/){
